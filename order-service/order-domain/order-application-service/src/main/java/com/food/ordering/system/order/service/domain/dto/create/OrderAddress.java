00ffeed34be24a8b0f72a276412d29a8620e836f
AustinKPathfinder <austin.keiber@pathfinderapps.com>
2022-10-15 16:52:56 -0400

    init

---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/dto/create/OrderAddress.java
@@ -0,0 +1,29 @@
+package com.food.ordering.system.order.service.domain.dto.create;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class OrderAddress {
+
+    @NotNull
+    @Max(value = 50)
+    private final String street;
+
+    @NotNull
+    @Max(value = 10)
+    private final String postalCode;
+
+    @NotNull
+    @Max(value = 50)
+    private final String city;
+
+}
